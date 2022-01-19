5dc16a70e843d02fc47d7126d6d214d4b7e92f02
Kazishayem <kazishayem1234@gmail.com>
2022-01-19 13:00:09 +0600

    updated

--- JS/script.js
+++ JS/script.js
@@ -3,20 +3,17 @@ let booklist = document.querySelector('#book-list');
 
 
 class Book{
-    constructor(title,author,isbn)  //perameter
+    constructor(title,author,isbn) 
     {
-         this.title = title;   //property
+         this.title = title;   
          this.author = author;
          this.isbn = isbn;
     }
 }
 
 class UI{
-    constructor()
-    {
-
-    }
-    addToBookList(book)
+    
+    static addToBookList(book)
     {
         let list = document.querySelector('#book-list');
         let row = document.createElement('tr');
@@ -30,14 +27,14 @@ class UI{
         list.appendChild(row);
         
     }
-    clearField()
+    static clearField()
     {
     document.querySelector('#title').value = '',
     document.querySelector('#author').value = '',
     document.querySelector('#isbn').value = '';
     }
 
-    showAlert(message , className)
+    static showAlert(message , className)
     {
         let div = document.createElement('div');
         div.className =`alert ${className}`;
@@ -52,11 +49,13 @@ class UI{
 
     }
 
-    deleteFormBook(target)
+    static deleteFormBook(target)
     {
         if(target.hasAttribute('href'))
         {
             target.parentElement.parentElement.remove();
+            UI.showAlert("Remove Book" , "error")
+            Store.removeBook(target.parentElement.previousElementsibling.textContent.trim())
 
 
         }
@@ -65,32 +64,84 @@ class UI{
     }
 }
 
+// local storage
+
+// class Store
+// {
+//     static getbooks()
+//     {
+//         let books;
+//         if(localStorage.getItem('books') === null)
+//         {
+//             books =[];
+
+//         }
+//         else{
+//             books = JSON.parse(localStorage.getItem('books'));
+//         }
+
+//         return books; 
+//     }
+
+//     static addBook(book)
+//     {
+//         let books = Store.getbooks();
+//         books.push(book);
+
+//         localStorage.setItem('books' ,JSON.stringify(books));
+
+//     }
+
+//     displayBooks()
+//     {
+//         let books = Store.getbooks();
+
+//         books.forEach(book => {
+//             UI.addToBookList(book);
+//         })
+//     }
+//     static removeBook(isbn)
+//     {
+//         let books = Store.getbooks();
+
+//         books.forEach((book , index) =>{
+//             if(book.isbn === isbn)
+//             {
+//                 books.splice(index , 1);
+//             }
+
+//         })
+//         localStorage.setItem('books' ,JSON.stringify(books))
+//     }
+// }
+
 //add event listenter
 
 form.addEventListener('submit' , newBook);
 booklist.addEventListener('click' , removeBook);
+Document.addEventListener('DOMContentLoaded' , Store.displayBooks());
 
 //define function
 
 function newBook(e)
 {
-    // console.log("hello");
+    
 
     let title = document.querySelector('#title').value ,
     author = document.querySelector('#author').value ,
     isbn = document.querySelector('#isbn').value;
 
     let book = new Book(title,author,isbn);
-    // console.log(book);
+    
 
-    let ui = new UI();
+    // let UI = new UI();
 
     if(title === '' || author === '' || isbn === '')
     {
-        // alert('Please Fillup All The Fields');
         
+        UI.showAlert("Please Fillup All The Fields" , "error")
 
-        ui.showAlert("Please Fillup All The Fields" , "error")
+
         
 
     }
@@ -100,10 +151,12 @@ function newBook(e)
         let book = new Book(title,author,isbn);
         
 
-        ui.addToBookList(book);
+        UI.addToBookList(book);
 
-        ui.clearField();
-        ui.showAlert("Book Added Successfully" , "success");
+        UI.clearField();
+        UI.showAlert("Book Added Successfully" , "success");
+
+        Store.addBook();
 
         
 
@@ -115,10 +168,10 @@ function newBook(e)
 
 function removeBook(e)
 {
-    let ui = new UI();
+    // let UI = new UI();
 
-    ui.deleteFormBook(e.target);
-    ui.showAlert("Remove Book" , "error")
+    UI.deleteFormBook(e.target);
+    // UI.showAlert("Remove Book" , "error")
 
 
     e.preventDefault();
